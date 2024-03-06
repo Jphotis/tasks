109af66a4d8667a6e7f3a218e7c63c09e8eecf57
jordanphotis <jpho@udel.edu>
2024-03-05 19:02:13 -0500

    Finished all of task 8

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [Answer, setAnswer] = useState<boolean>(false);
+    function showAnswer(): void {
+        setAnswer(!Answer);
+    }
+    return (
+        <div>
+            <Button onClick={showAnswer}>Reveal Answer</Button>{" "}
+            {Answer && <div>42</div>}
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,23 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [die1Val, setDie1Val] = useState<number>(0);
+    const [die2Val, setDie2Val] = useState<number>(1);
+    function rollDice() {
+        setDie1Val(d6());
+        //setDie2Val(d6());
+    }
+    function rollDice2() {
+        setDie2Val(d6());
+        //setDie2Val(d6());
+    }
+    return (
+        <div>
+            <span data-testid="left-die"> {die1Val}</span>
+            <span data-testid="right-die"> {die2Val}</span>
+            <Button onClick={rollDice}> Roll Left</Button>
+            <Button onClick={rollDice2}> Roll Right </Button>
+            {die1Val == die2Val && (die1Val != 1 ? "Win" : "Lose")}
+        </div>
+    );
 }
